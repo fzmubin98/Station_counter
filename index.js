80c4f9605f2a772cfcaa147b5753f870381454d6
fzmubin98 <fzmubin98@gmail.com>
2023-01-11 13:26:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function decrement(){
+    count += - 1
+    countEl.textContent = count
+}
+
+function save(){
+    let saved = count + " --> "
+    saveEl.textContent += saved
+    countEl.textContent = 0
+    count = 0
+}
